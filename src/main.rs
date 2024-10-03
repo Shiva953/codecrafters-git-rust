a3e44c7379d1f13ba3690761d08056a207049765
Shiva953 <b22070@students.iitmandi.ac.in>
2024-10-03 22:11:53 +0530

    creating blob attempt 1

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 #[allow(unused_imports)]
 use std::env;
+use std::fmt::format;
 #[allow(unused_imports)]
 use std::fs;
 use flate2::read::ZlibDecoder;
+use flate2::write::ZlibEncoder;
+use flate2::Compression;
 use std::ffi::CStr;
 use std::io::prelude::*;
+use std::io::Write;
+use std::path::Path;
+use sha1::{Digest, Sha1};
 
 //[CONTINUATION PROJECT] - IMPLEMENTATING GIT FROM SCRATCH
 fn main() {
@@ -30,6 +36,7 @@ fn main() {
         // Step 1: identify the file & read from it
         //object directory is in form of .git/objects/[first 2 hash digits]/[remaining hash digits after that]
         //ex - .git/objects/e8/8f7a929cd70b0274c4ea33b209c97fa845fdbc
+        //command = git cat-file -p <blobhash>, args[3] = blobhash, args[3][..2] = first 2 digits(blob object folder), args[3][2..] = actual blob object FILE
         let content = fs::read(format!(".git/objects/{}/{}", &args[3][..2], &args[3][2..])).unwrap();
 
         //Step2: Decompress using Zlib
@@ -46,7 +53,45 @@ fn main() {
             None => print!("No content found"),
         }
 
-        //create the blob object
+    } else if(args[1] == "hash-object"){
+        // replicating command "git hash-object -w hello.txt"
+        // create the blob file
+        // ex file hello.txt
+
+        // STEP 1 - TAKE ORIGINAL CONTENTS OF EXAMPLE FILE
+        let filename = args[3];
+        let path = format!("./{}", filename);
+        let content = fs::read_to_string(path.clone());
+        let sha_input = match fs::read_to_string(path.clone()){
+            Ok(x) => {
+                let size = fs::metadata(path.clone())?.len();
+                format!("blob {}\0{}", size, x)
+            },
+            Err(e) => {
+                "invalidfilecontent".to_string()
+            }
+        };
+
+        // STEP 2 - COMPUTE THE SHA1HASH(blob <size>\0<original file contents>)
+        let mut hasher = Sha1::new();
+        hasher.update(sha_input.clone());
+        let mut hash = hasher.finalize();
+
+        // STEP 3 - CREATE THE .git/objects/[hash[..2]]/[hash[2..]] file
+        let dir_name = &hash[..2];
+        let file_name = &hash[2..];
+        // Create the full path .git/objects/[hash[..2]]/[hash[2..]]
+        let dir_path = format!(".git/objects/{}", dir_name);
+        let file_path = format!("{}/{}", dir_path, file_name);
+        // create directory for blob object
+        fs::create_dir_all(&dir_path)?;
+        // Create blob object file
+        let mut blob_object_file = fs::File::create(file_path)?;
+
+        // STEP 4 - COMPRESS THE CONTENTS OF THE ORIGINAL FILE USING ZLIB AND WRITE IT TO THE .git/objects/[hash[..2]]/[hash[2..]] file
+        let mut encoder = ZlibEncoder::new(blob_object_file, Compression::default());
+        encoder.write_all(content.unwrap().as_bytes());
+        encoder.finish()?;
     }
     else {
         println!("unknown command: {}", args[1])
